5a94481284db192b01f4b0ddead15e2241cb2958
idabagusangga <angga.idabagus@gmail.com>
2017-11-15 10:41:02 +0700

    nested arrays recursive

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-function createNestedArr(jumlahCol,JumlahRow){
-  let arr = []
-  let abjad = 'abcdefghijklmnopqrstuvwxyz'
-  for(let i = 0; i <= JumlahRow; i++){
-    arr.push([]);
-    for(let j = 0; j <= jumlahCol; j++){
-      let huruf = abjad.charAt(Math.floor(Math.random() * abjad.length));
-      arr[i].push(huruf)
-    }
-  }
-  console.log(arr)
-}
+// function createNestedArr(jumlahCol,JumlahRow){
+//   let arr = []
+//   let abjad = 'abcdefghijklmnopqrstuvwxyz'
+//   for(let i = 0; i <= JumlahRow; i++){
+//     arr.push([]);
+//     for(let j = 0; j <= jumlahCol; j++){
+//       let huruf = abjad.charAt(Math.floor(Math.random() * abjad.length));
+//       arr[i].push(huruf)
+//     }
+//   }
+//   console.log(arr)
+// }
 
 //driver
-createNestedArr(4,3)
+
+
+
+function createNestedArr(jumlahCol,jumlahRow,arr = []){
+  let abjad = 'abcdefghijklmnopqrstuvwxyz';
+
+  if(jumlahRow == 0){
+    console.log(arr)
+    return arr
+  }
+  else{
+    var arrHuruf = []
+    while(arrHuruf.length <= jumlahCol){
+      let huruf = abjad.charAt(Math.floor(Math.random() * abjad.length));
+      arrHuruf.push(huruf)
+    }
+
+  }
+  arr.push(arrHuruf)
+  return createNestedArr(jumlahCol,jumlahRow - 1,arr)
+}
+
+
+//driver
+
+createNestedArr(5,3)
