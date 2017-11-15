a5c852eccc2f2c05072c15a750073d57647d70aa
ariefmanda <arief.manda57@gmail.com>
2017-11-15 11:34:03 +0700

    Done Evaluate Random

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const random = 'asdfghjklqwertysdfxcgvhbn';
+const random = 'abcdefghijklmnopqrstuvwxyz';
 function createNestedArray(jumlahRow, jumlahCol) {
     let array = [];
-    let randomCount = 0;
     for (let i = 0; i < jumlahRow; i++) {
         array.push([]);
         for (j = 0; j < jumlahCol; j++) {
-            array[i].push(random[randomCount]);
-            randomCount++;
+            array[i].push(random[Math.floor(Math.random() * random.length)]);
         }
     }
+
     return array;
 }
+// function randomm(){
+//     randomCount++;
+//     return randomCount
+// }
+
 // var nestedArray = (jumlahRow,jumlahCol)=>{
 //     let array = [];
 //     let randomCount = 0;
