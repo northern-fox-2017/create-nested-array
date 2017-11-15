7c9774460290ecc9047ce15e134a3877c90a1817
rezaalfarabi <anangreza18@gmail.com>
2017-11-15 15:31:04 +0700

    revisi release 0

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function createNestedArr (jumlahCol, jumlahRow) {
+function createNestedArr (jumlahRow, jumlahCol) {
   // YOUR CODE HERE
   let abjad = 'abcdefghijklmnopqrstuvwxyz'
   let arr = [];
 
+
+//revisi
+//ini untuk menentukan jumlah baris
   for (let i = 0; i < jumlahRow; i++) {
     arr.push([])
     // console.log(arr)
+
+    // dan ini untuk menentukan jumlah kolom
     for (let j = 0; j < jumlahCol; j++) {
     arr[i].push(abjad[Math.floor(Math.random() * abjad.length)])
     }
@@ -14,5 +19,5 @@ function createNestedArr (jumlahCol, jumlahRow) {
 }
 
 console.log(createNestedArr(5,3))
-console.log(createNestedArr(3,4))
-console.log(createNestedArr(6,2))
+// console.log(createNestedArr(3,4))
+// console.log(createNestedArr(6,2))
